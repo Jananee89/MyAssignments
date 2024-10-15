4ee9c47564c5fd20094c0b25714594257ba2acdf
Jananee89 <jananee89@gmail.com>
2024-10-15 18:56:07 +0530

    Add files via upload

---
+++ day1/Chrome.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class Chrome {
+	public static void main(String[] args) {
+		float version=91.0f;
+		String developer="google";
+		boolean isBeta=false;
+		int releaseYear=2008;
+		char shortcutKey='C';
+		System.out.println("Version is:" +version);
+		System.out.println("Developer is:" +developer);
+		System.out.println("Boolean is:" +isBeta );
+		System.out.println("Release year is:" +releaseYear);
+		System.out.println("Shortcut Key is:" +shortcutKey);
+		
+		
+	}
+}
---
+++ day2/IsPrime.java
@@ -0,0 +1,33 @@
+package week1.day2;
+
+import java.util.Iterator;
+
+public class IsPrime {
+
+	public static void main(String[] args) {
+		int n=2;
+		System.out.println("The number is:" +n);
+		int count=0;
+		if(n==0 || n==1)
+		{
+			System.out.println(n+ " Number is neither prime not composite");
+			return;
+		}
+		for(int i=1;i<=n;i++) {
+			
+			if (n%i==0) {
+				count++;
+			}
+		}
+			if (count>2) {
+				System.out.println(n+ " Number is not a prime number");
+			}
+			else {
+				System.out.println(n+ " Number is a prime number");
+			}
+		
+		
+
+	}
+
+}
