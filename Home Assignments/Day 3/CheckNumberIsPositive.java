aca9f92118a5dc196bb0f02b355b99eb9bc9b587
Jananee89 <jananee89@gmail.com>
2024-10-21 10:03:22 +0530

    Add files via upload
    home assignments week1

---
+++ Home Assignments/Day 3/CheckNumberIsPositive.java
@@ -0,0 +1,19 @@
+package week1.day3;
+
+public class CheckNumberIsPositive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int val=10;
+		if(val>0) {
+			System.out.println(val+":is a positive number");
+		}
+		else if(val<0) {
+			System.out.println(val+ " :is a negative number");
+		}
+		else {
+			System.out.println(val+ " :is neutral");
+		}
+	}
+
+}
